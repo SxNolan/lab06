724b9d0e76e358e4f534f3f5b02eed9088712a59
Christian Luca <Giovanni.luca@studio.unibo.it>
2023-10-30 21:41:30 +0100

    fatta buona parte di esercizio 65. Sembra fatto bene ma bisogna ancora darci una guardata attenta.

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/UseGraph.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/UseGraph.java
@@ -3,13 +3,14 @@ package it.unibo.generics.graph;
 import it.unibo.generics.graph.api.Graph;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 /**
  *
  */
-public final class UseGraph {
+public final class UseGraph implements Graph{
 
     private UseGraph() {
     }
@@ -24,6 +25,8 @@ public final class UseGraph {
          */
         testGraph(null);
     }
+
+    Graph<String> myGraph = null;
 
     private static void testGraph(final Graph<String> graph) {
         graph.addNode("a");
@@ -67,5 +70,39 @@ public final class UseGraph {
 
     private static String[] splitOnWhiteSpace(final String target) {
         return target.split("\\s+");
+    }
+
+    @Override
+    public void addNode(Object node) {
+        if (node == null || myGraph.nodeSet().contains(node)) {
+            return;
+        } else {
+            myGraph.nodeSet().add((String) node);
+        }
+    }
+
+    @Override
+    public void addEdge(Object source, Object target) {
+        if (target == null || source == null) {
+            return;
+        } else {
+            
+        }
+    }
+
+    @Override
+    public Set<String> nodeSet() {
+        return this.myGraph.nodeSet();
+    }
+
+    @Override
+    public Set<String> linkedNodes(Object node) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'linkedNodes'");
+    }
+
+    @Override
+    public List<String> getPath(Object source, Object target) {
+        return null;
     }
 }
